18a93cef42c7f8bd49c01db59a027a59d675d1d2
raul_chamorro <rchamo01@gmail.comDragonbol_11qgit config --global user.email rchamo01@gmail.com>
2023-01-03 18:39:50 +0100

    pruebas 3/1

--- scripts/bajar_zooniverse1.py
+++ scripts/bajar_zooniverse1.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import warnings
 
 
 os.system('panoptes project download 19814 datos1.csv')
@@ -9,7 +10,7 @@ os.system('mv datos1.csv datos/')
 data = pd.DataFrame()
 data = pd.read_csv('datos/datos1.csv')
 
-df = pd.DataFrame(columns=['index', 'subject_ids', 'Meteoro_No', 'Trayectoria', 'Grosor', 'Que_es'])
+df = pd.DataFrame(columns=['index', 'subject_ids', 'Meteoro_No', 'Trayectoria_1', 'Trayectoria_2', 'Grosor_1', 'Grosor_2', 'Que_es'])
 
 def buscar_si (frase):
 	aux = False
@@ -27,20 +28,18 @@ def buscar_si (frase):
 			salida = False
 	return salida
 	
-def buscar_primer_x1 (frase):
-	j = frase.find('x1')
-	l = frase.find('tool')
+def buscar_coordenadas(frase, stri, pos):
 	out = ''
-	for i in range(j, l-1):
+	if (pos == 1):
+		j = frase.find(stri)
+		l = frase.find(',', j)
+	elif(pos == 2):
+		j = frase.find(stri, frase.find(stri) + 100)
+		l = frase.find(',', j)
+	for i in range(j+4, l-1):
 		out = out + frase[i]
-	return out
-
-def buscar_segundo_x1 (frase):
-	j = frase.find('x1', frase.find('x1')+100)
-	l = frase.find('tool', frase.find('tool')+100)
-	out = ''
-	for i in range(j, l-1):
-		out = out + frase[i]
+	if out == '':
+		out = 'nan'
 	return out
 	
 def buscar_otro (frase):
@@ -53,6 +52,7 @@ def buscar_otro (frase):
 	
 ######################################################
 # Crear un nuevo dataFrame con datos especificos
+warnings.simplefilter("ignore")
 for i in range(len(data)):
 	fecha = data['created_at'].iloc[i]
 	if fecha[5:10] == '11-17':
@@ -62,15 +62,21 @@ for i in range(len(data)):
 		frase = data['annotations'].iloc[i]
 		if buscar_si(frase):
 			fila.append('Si')
-			fila.append(buscar_primer_x1(frase))
-			fila.append(buscar_segundo_x1(frase))
+			fila.append([float(buscar_coordenadas(frase, 'x1', 1)), float(buscar_coordenadas(frase, 'y1', 1))])
+			fila.append([float(buscar_coordenadas(frase, 'x2', 1)), float(buscar_coordenadas(frase, 'y2', 1))])
+			fila.append([float(buscar_coordenadas(frase, 'x1', 2)), float(buscar_coordenadas(frase, 'y1', 2))])
+			fila.append([float(buscar_coordenadas(frase, 'x2', 2)), float(buscar_coordenadas(frase, 'y2', 2))])
 			fila.append('-')
 		else:
 			fila.append('No')
 			fila.append('-')
 			fila.append('-')
+			fila.append('-')
+			fila.append('-')
 			fila.append(buscar_otro(frase))
-		df = df.append({'index':fila[0], 'subject_ids': int(fila[1]), 'Meteoro_No': fila[2], 'Trayectoria': fila[3], 'Grosor': fila[4], 'Que_es': fila[5]}, ignore_index=True)
+		df = df.append({'index':fila[0], 'subject_ids': int(fila[1]), 'Meteoro_No': fila[2], 'Trayectoria_1': fila[3], 'Trayectoria_2': fila[4], 'Grosor_1': fila[5], 'Grosor_1': fila[6], 'Que_es': fila[7]}, ignore_index=True)
+
+warnings.simplefilter("default")
 	
 ##########################################################
 # Clasificar datos	
@@ -124,7 +130,7 @@ for i in df['subject_ids'].unique():
 	dic[i] = [Nub/serie2.shape[0], Avio/serie2.shape[0], Bich/serie2.shape[0], Met/serie2.shape[0], Otr/serie2.shape[0]]
 	
 ##########################################################
-# Graficar
+# Graficar Meteoritos
 auc = pd.DataFrame(dic)
 fig, ax = plt.subplots()
 
@@ -139,3 +145,39 @@ ax.set_xticklabels(auc.columns)
 plt.xticks(auc.columns, rotation=90)
 plt.show()
 
+##########################################################
+# Graficar Trayectorias
+warnings.simplefilter("ignore")
+dic2 = {}
+for i in df['subject_ids'].unique():
+	serie = df[df.subject_ids == i].Meteoro_No == 'Si'
+	s = 0
+	for j in serie.index:
+		if serie[j] == True:
+			s = s + 1
+	if s/serie.shape[0] < 0.8: #Descarta los que tienen mas del 80% como meteoritos
+		continue
+	x1 = []
+	y1 = []
+	for j in df[df.subject_ids == i]['Trayectoria_1']:
+		if j[0] == '-' or j[0] == 'nan':
+			continue
+		x1.append(j[0])
+		y1.append(j[1])
+	x2 = []
+	y2 = []
+	for j in df[df.subject_ids == i]['Trayectoria_2']:
+		if j[0] == '-' or j[0] == 'nan':
+			continue
+		x2.append(j[0])
+		y2.append(j[1])
+	plt.scatter(x1, y1, label = i)
+	plt.scatter(x2, y2, label = i)
+plt.legend(bbox_to_anchor = (1,0), loc = 'best')
+plt.show()
+warnings.simplefilter("default")
+
+##########################################################
+# Mostrar informacion
+
+print(data[data.subject_ids == 82613666]['subject_data'].iloc[0])
